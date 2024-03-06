7c48d298ce7c6f6e5113e9d78d4de238fa47d11f
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2024-03-07 01:27:41 +0700

    initial project

---
+++ src/main/java/com/example/ceom/CeomApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ceom/CeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
